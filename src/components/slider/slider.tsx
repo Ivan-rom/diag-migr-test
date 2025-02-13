90f6d9c59120192cf6a37c9a1547cd33689db49a
Ivan-rom <rom_1van@icloud.com>
2025-02-13 13:27:52 +0600

    feat: implement slider behavior according to thumbnail

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -14,7 +14,7 @@ type SliderElement = {
 const elements: SliderElement[] = [
   {
     id: 1,
-    img: "#",
+    img: "vodka-ketelone.png",
     name: "Bottle",
     description:
       "A crisp, smooth taste with a splash of citrus and long finish",
@@ -64,27 +64,24 @@ function Slider() {
     <div className="flex items-center">
       <button
         onClick={prevElementHandler}
-        className="absolute left-1/3 z-10 p-5 rounded-full border border-black">
+        className="absolute left-[20%] z-10 p-5 rounded-full border border-black">
         <img
           src="/arrow-curved.svg"
           alt="left"
           className="rotate-180 size-10"
         />
       </button>
-      <div className="wrapper w-[90%] mx-auto overflow-hidden">
+      <div className="wrapper w-[100%] mx-auto overflow-hidden">
         <ul
-          className={`flex justify-between w-[100%] duration-300 ease-linear translate-x-[-${
-            activeElementIndex * 100
-          }%]`}>
+          className={`flex justify-center w-[100%] mx-auto duration-300 ease-in-out translate-x-0`}
+          style={{ translate: `${50 - activeElementIndex * 50}%` }}>
           {elements.map(({ id, name, img, description, infoLink, buyLink }) => (
-            <li key={id} className="flex flex-col items-center min-w-[100%]">
+            <li key={id} className="flex flex-col items-center min-w-[50%]">
               <img src={img} alt={name} className="h-80 block" />
 
               {elements[activeElementIndex].id === id && (
                 <div className="text-center">
-                  <h3 className="font-bold text-xl uppercase mb-2">
-                    {name} {id}
-                  </h3>
+                  <h3 className="font-bold text-xl uppercase mb-2">{name}</h3>
                   <p className="mb-6">{description}</p>
                   <div className="flex justify-center gap-3">
                     <a
@@ -106,7 +103,7 @@ function Slider() {
       </div>
       <button
         onClick={nextElementHandler}
-        className="absolute right-1/3 z-10 p-5 rounded-full border border-black">
+        className="absolute right-[20%] z-10 p-5 rounded-full border border-black">
         <img src="/arrow-curved.svg" alt="right" className="size-10" />
       </button>
     </div>
