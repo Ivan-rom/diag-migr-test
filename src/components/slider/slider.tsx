bfd6fb7c45e1fb69d1a16b8c7561dbb13ae34c70
Ivan-rom <rom_1van@icloud.com>
2025-02-14 12:32:40 +0600

    feat: add gradient for side elements in slider

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -66,15 +66,19 @@ function Slider() {
   }
 
   return (
-    <div className="flex items-center">
+    <div
+      className="flex items-center relative
+    before:content-[''] before:h-full before:w-32 before:block before:absolute before:left-0 before:bg-gradient-to-r before:from-lime-100 before:to-transparent before:z-10
+    after:content-[''] after:h-full after:w-32 after:block after:absolute after:right-0 after:bg-gradient-to-l after:from-lime-100 after:to-transparent after:z-10
+    ">
       <button
         onClick={prevElementHandler}
-        className="absolute left-[20%] z-10 p-5 rounded-full border border-black">
+        className="absolute left-[20%] z-20 p-5 rounded-full border border-black">
         <Image src={arrow} alt="left" className="rotate-180 size-10" />
       </button>
-      <div className="wrapper w-[100%] mx-auto overflow-hidden">
+      <div className="w-[100%] mx-auto overflow-hidden">
         <ul
-          className={`flex justify-center items-center w-[100%] mx-auto duration-500 ease-in-out translate-x-0`}
+          className="flex justify-center items-center w-[100%] mx-auto duration-500 ease-in-out translate-x-0"
           style={{ translate: `${50 - activeElementIndex * 50}%` }}>
           {elements.map(({ id, name, img, description, infoLink, buyLink }) => (
             <li key={id} className="flex flex-col items-center min-w-[50%]">
@@ -109,7 +113,7 @@ function Slider() {
       </div>
       <button
         onClick={nextElementHandler}
-        className="absolute right-[20%] z-10 p-5 rounded-full border border-black">
+        className="absolute right-[20%] z-20 p-5 rounded-full border border-black">
         <Image src={arrow} alt="right" className="size-10" />
       </button>
     </div>
