a2b31c1ea9881206bab9e9c26f6f3ad351765fab
Ivan-rom <rom_1van@icloud.com>
2025-02-14 10:55:31 +0600

    feat: implement product description transition

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -74,14 +74,19 @@ function Slider() {
       </button>
       <div className="wrapper w-[100%] mx-auto overflow-hidden">
         <ul
-          className={`flex justify-center items-center w-[100%] mx-auto duration-300 ease-in-out translate-x-0`}
+          className={`flex justify-center items-center w-[100%] mx-auto duration-500 ease-in-out translate-x-0`}
           style={{ translate: `${50 - activeElementIndex * 50}%` }}>
           {elements.map(({ id, name, img, description, infoLink, buyLink }) => (
             <li key={id} className="flex flex-col items-center min-w-[50%]">
               <Image src={img} alt={name} className="size-96 mb-10" />
 
-              {elements[activeElementIndex].id === id && (
-                <div className="text-center">
+              <div
+                className={`text-center duration-300 grid grid-rows-[0fr] opacity-0 ${
+                  elements[activeElementIndex].id === id &&
+                  "grid-rows-[1fr] opacity-100" // for smooth height changing
+                  // "bouncing" without additional content below
+                }`}>
+                <div className="overflow-hidden">
                   <h3 className="font-bold text-xl uppercase mb-2">{name}</h3>
                   <p className="mb-6">{description}</p>
                   <div className="flex justify-center gap-3">
@@ -97,7 +102,7 @@ function Slider() {
                     </a>
                   </div>
                 </div>
-              )}
+              </div>
             </li>
           ))}
         </ul>
